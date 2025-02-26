c3968e62abc5dc0fff626c4d296290af11c8467f
Roshni  Bhuva <roshnibhuva6@gmail.com>
2025-02-26 11:34:34 +0530

    Update prime.js

--- udf & logic/prime or not/prime.js
+++ udf & logic/prime or not/prime.js
@@ -1,18 +1,27 @@
-function prime(n){
-    let i;
-  
-    for(i=2; i<=n; i++){
-      if(i==0){
-          document.write("num is prime number");
+// wap to check passed number in udf prime or not.
+function prime(n)
+{
+    
+ let x = 1; 
+    
+    
+      if(n%2==1){
+          document.write(n + " is prime number");
       }
-      else{
-          document.write("num is not prime number");
+      else if(n==2)
+      {
+        document.write(n + " is prime number");
       }
-    }
+      
+      else
+      {
+          document.write(n + " is not prime number");
+      }
+    
   
   }
   
-  let n = prompt("Enter the N is prime :");
-  
-  n=parseInt(n);
-  prime(n);
+  let n = parseInt(prompt("Enter the N is prime :"));
+   
+
+   prime(n);
