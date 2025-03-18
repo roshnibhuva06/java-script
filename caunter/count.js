34864b980c71acf92750d79e5f17cb952e6d0e7e
Roshni  Bhuva <roshnibhuva6@gmail.com>
2025-03-18 22:27:51 +0530

    Add files via upload

---
+++ caunter/count.js
@@ -0,0 +1,48 @@
+const title = document.getElementById("title");
+const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
+const btn3 = document.getElementById("btn3");
+const btn4 = document.getElementById("btn4");
+
+let count=0;
+
+btn1.addEventListener("click",()=>{
+    count++;
+    title.innerHTML = count;
+});
+
+
+btn2.addEventListener("click",()=>{
+    if(count > 0)
+    {
+        count--;
+    }
+    title.innerHTML = count;
+});
+
+
+btn3.addEventListener("click",()=>{
+ if(count==0)
+ {
+    title.innerHTML ="you are not multiply 0";
+ }
+ else {
+    count *=2;
+    title.innerHTML = count;
+ }
+    
+});
+
+btn4.addEventListener("click",()=>{
+    if(count==0)
+        {
+           title.innerHTML ="you are not multiply 0";
+        }
+        else {
+           count /=2;
+           title.innerHTML = count;
+        }
+           
+    
+});
+
