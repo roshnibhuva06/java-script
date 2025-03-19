817963136cd313f73951c085788d0df25095c949
Roshni  Bhuva <roshnibhuva6@gmail.com>
2025-03-19 21:59:57 +0530

    Add files via upload

---
+++ caunter/que2/calcu.js
@@ -0,0 +1,9 @@
+const one = document.getElementById("one")
+const two = document.getElementById("two")
+const btn1 = document.getElementById("btn1")
+const h2 = document.getElementById("h2")
+
+
+
+
+
