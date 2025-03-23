8f9f2a0d5b8057eb4ae8c94f51d6584459f8bea1
Roshni  Bhuva <roshnibhuva6@gmail.com>
2025-03-23 18:00:19 +0530

    Add files via upload

---
+++ background/color.js
@@ -0,0 +1,25 @@
+const background = document.querySelector("#background")
+const body = document.querySelector("body")
+const code = document.querySelector("#code")
+const button = document.querySelector("#button")
+
+
+background.addEventListener("click",() =>{
+    changebackgroundcolor();
+});
+
+
+ 
+
+function changebackgroundcolor(){
+
+    const randomnum  = Math.floor(Math.random() * (256) * (256) * (256))
+    const randomcolor = "#" + randomnum.toString(16);
+    console.log(randomcolor);
+    body.style.backgroundColor = randomcolor;
+    code.innerHTML = randomcolor ;
+}
+
+ 
+
+ 
